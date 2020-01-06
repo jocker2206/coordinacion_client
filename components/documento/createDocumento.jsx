4bc91b9c15a5156348fb3a86b50425015f1bb298
Hans Medina <twd2206@gmail.com>
2020-01-06 14:15:09 -0500

    casi listo

--- components/documento/createDocumento.jsx
+++ components/documento/createDocumento.jsx
@@ -8,14 +8,27 @@ export default class CreateDocumento extends Component
 
 
     state = {
-        cod_alumno: ""
+        cod_alumno: "",
+        type_request_id: "",
+        type_documento_id: "",
+        observacion: "",
+        monto: 0
     };
+
+
+    handleInput = (e) => {
+        let { name, value } = e.target;
+        this.setState({ [name]: value }); 
+    }
 
 
     handleCodAlumno = (e) => {
         let { value, name } = e.target;
-        if (value.length <= 10 && typeof value == "number") {
-            this.setState({ [name]: value });
+        if (value.length <= 10) {
+            let regex = /^[0-9]+$/
+            if (regex.test(value) || value == "") {
+                this.setState({ [name]: value });
+            }
         }
     }
 
@@ -40,8 +53,10 @@ export default class CreateDocumento extends Component
 
                     <div className="form-group">
                         <b>Tip. Documento</b>
-                        <select name="" 
+                        <select name="type_documento_id" 
                             className="form-control"
+                            value={state.type_documento_id}
+                            onChange={this.handleInput}
                         >
                             <option value="">Select. Tip. Documento</option>
                         </select>
@@ -49,21 +64,59 @@ export default class CreateDocumento extends Component
 
                     <div className="form-group">
                         <b>Tip. Solicitud</b>
-                        <select name="" 
+                        <select name="type_request_id" 
                             className="form-control"
+                            value={state.type_request_id}
+                            onChange={this.handleInput}
                         >
                             <option value="">Select. Tip. Solicitud</option>
                         </select>
                     </div>
 
                     <div className="form-group">
-                        <b>Codigo de Estudiante</b>
+                        <div className="row">
+                            <div className="col-md-4">
+                                <b>Codigo de Estudiante</b>
+                                <input type="text" 
+                                    className="form-control"
+                                    name="cod_alumno"
+                                    value={state.cod_alumno}
+                                    onChange={this.handleCodAlumno}
+                                />
+                            </div>
+                            <div className="col-md-8">
+                                <b>Nombre Completo</b>
+                                <input type="text" 
+                                    className="form-control"
+                                    disabled={true}
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <b>N° Recibo</b>
                         <input type="text" 
                             className="form-control"
                             name="cod_alumno"
-                            value={state.cod_alumno}
-                            onChange={this.handleCodAlumno}
+                            value={state.numero_recibo}
                         />
+                    </div>
+
+                    <div className="form-group">
+                        <b>Monto</b>
+                        <input type="number"
+                            step="any" 
+                            className="form-control"
+                            name="cod_alumno"
+                            value={state.monto}
+                        />
+                    </div>
+
+                    <div className="form-group text-right">
+                        <button className="btn btn-primary">
+                            <i className="fas fa-save"></i> Guardar
+                        </button>
                     </div>
                 </Card.Body>
             </Modal>
